7958a036f59f742786cb144932837fc7966e14f0
deepakshimpi001 <deepakshimpi001@gmail.com>
2023-03-26 11:03:22 +0530

    7th vhanges

--- database connect to python.py
+++ database connect to python.py
@@ -2,8 +2,11 @@ import mysql.connector
 
 conn=mysql.connector.connect(user='root',host="localhost",password="9552207859",database="hr")
 
-print(conn)
+#print(conn)
 
 cursorobject=conn.cursor()
+try:
+    cursorobject.execute("create database geeksforgeek")
 
-cursorobject.execute("create database geeksforgeek")
+except:
+    print("database alredy created")
